c1e0b854ec79309546d9ca99a1c46fd246408b70
Álvaro Aberastain Marzorati <a.aberastain.marzorati@gmail.com>
2025-11-27 20:46:52 -0300

    fix

--- api-cloud/src/main/java/grupo16/dssd/api_cloud/config/SecurityConfig.java
+++ api-cloud/src/main/java/grupo16/dssd/api_cloud/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                                         "/v3/api-docs/**",
                                         "/swagger-ui/**",
                                         "/swagger-ui.html",
-                                        "/api/v1/system").permitAll() // login/register
+                                        "/api/v1/system/**").permitAll() // login/register
                                 .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
